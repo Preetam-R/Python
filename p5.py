8d7c8c1c03f328f9cb5fdefcd6fdc22ce2c92fbf
Preetam Rathod <rathodpreetam613@gmail.com>
2026-02-19 15:33:22 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,9 @@
+#Python Program to Check if a Number is Positive, Negative or 0
+
+num = float(input("enter the number: "))
+if(num==0):
+    print("Its zero:", num)
+elif(num>0):
+    print("Its a positive number:",num)
+else:
+    print("Its a negetive number:",num)
