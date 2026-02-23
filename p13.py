94f629f557a779b627fab6c964082a75a92457ec
Preetam Rathod <rathodpreetam613@gmail.com>
2026-02-23 20:57:22 +0530

    Add files via upload

---
+++ p13.py
@@ -0,0 +1,13 @@
+
+marks = {}
+
+m1 = input("enter the marks of phy:")
+m2 = input("enter the marks of math:")
+m3 = input("enter the marks of chem")
+
+marks.update({"phy": m1,
+                "math": m2,
+                "chem":m3})
+
+print(marks)
+
