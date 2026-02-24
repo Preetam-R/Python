dd907d1cad182429c833dcf71fe939e11e965e19
Preetam Rathod <rathodpreetam613@gmail.com>
2026-02-24 21:23:47 +0530

    Add files via upload

---
+++ p15.py
@@ -0,0 +1,31 @@
+#by using while loop print the numbers from 1to 100
+
+# i = 1
+
+# while(i<=100):
+#     print(i)
+#     i = i +1
+
+#by using while loop print the numbers from 100 to 1
+
+# p = 100
+# while(p>=1):
+#print(p)
+# p = p-1
+
+
+
+#write a program to print the multiplication table
+
+# i = 1
+# n = int(input("enter the number "))
+
+# while(i<=10): #elements using the while loop
+
+tb = [1,4,9,16,25,36,48,64,81,100]
+print(len(tb))
+i = 0
+while(i<=len(tb)-1):
+    print(tb[i])
+    i = i+1
+    
