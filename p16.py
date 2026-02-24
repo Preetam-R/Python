6ef57762e69ec8fb02049d6ead328039858b1deb
Preetam Rathod <rathodpreetam613@gmail.com>
2026-02-24 21:24:01 +0530

    Add files via upload

---
+++ p16.py
@@ -0,0 +1,18 @@
+# Write a python program to find an average of two numbers entered by the user.
+
+n1 = int(input("Enter the 1st number:"))
+n2 = int(input("Enter the 2nd number:"))
+
+sum = n1 + n2
+
+avg = sum/2
+print(avg)
+
+#Write a python program to calculate the square of a number entered by the user
+
+num = int(input("Enter the number to square of the number:"))
+
+square = num**2
+
+print(f"the Square of {num} is = {square}")
+
