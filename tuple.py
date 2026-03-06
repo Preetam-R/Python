6752d8b3869371746b8826f7956833587d82bea6
Preetam Rathod <rathodpreetam613@gmail.com>
2026-03-06 21:39:34 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+#its imuatable data type unlike list
+#instead of [] we should use() to create tuple
+
+tup = (3, 4, 9)
+print(tup)
+
+print(tup[2])
+
+#we can use slicing like list 
+
+#methods of tuple
+print(tup.index(1)) #this prints particular index
+
