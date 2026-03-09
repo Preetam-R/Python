c7e2fbec82c1f49406355c159451b6337104c835
Preetam Rathod <rathodpreetam613@gmail.com>
2026-03-09 22:42:00 +0530

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,7 +1,9 @@
-#even and odd
-num = input("enter the number:")
-num = int(num)
-if num % 2==0:
-    print("this is even number")
-else:
-    print("this is odd number")
+#Create a 3×3 array filled with zeros and a 3×3 array filled with ones.
+
+import numpy as np
+print("The 3X3 zero array:")
+zero = np.zeros((3,3))
+print(zero)
+print("The 1X1 ones array:")
+one = np.ones((3,3))
+print(one)
