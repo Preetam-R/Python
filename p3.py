67cf7fcce3006538630d90d4b5a76c1721de4dc2
Preetam Rathod <rathodpreetam613@gmail.com>
2026-03-09 22:42:22 +0530

    Add files via upload

--- p3.py
+++ p3.py
@@ -1,12 +1,5 @@
-# Question: Write a Python program to find the largest among three numbers entered by the user.
+#Create a 1D array of 10 evenly spaced values between 5 and 50
+import numpy as np
 
-n1 = float(input("enter the number1:"))
-n2 = float(input("enter the number2:"))
-n3 = float(input("enter the number3:"))
-
-if (n1>n2 and n1>n3):
-    print(f"Number1 {n1} is greater ")
-elif (n2>n1 and n2>n3):
-    print(f"number 2 {n2} is greater ")
-else:
-    print(f"number3 {n3} is greater ")
+lin = np.linspace(5,50, num = 10,dtype = int)
+print(lin)
